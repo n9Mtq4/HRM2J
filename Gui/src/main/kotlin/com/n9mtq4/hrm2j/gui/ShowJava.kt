0a59291d28627d4ae9fd6c40cc1e3ac861974d3a
n9Mtq4 <bill.wresnahan@gmail.com>
2016-07-28 20:16:01 -0400

    implemented showing the java

---
+++ Gui/src/main/kotlin/com/n9mtq4/hrm2j/gui/ShowJava.kt
@@ -0,0 +1,39 @@
+package com.n9mtq4.hrm2j.gui
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants
+import org.fife.ui.rtextarea.RTextScrollPane
+import javax.swing.JFrame
+
+/**
+ * Created by will on 7/28/16 at 4:36 PM.
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+class ShowJava(val javaStr: String) {
+	
+	val frame: JFrame
+	
+	init {
+		
+		frame = JFrame("Java")
+		
+		val textArea = RSyntaxTextArea(javaStr).apply {
+			syntaxEditingStyle = SyntaxConstants.SYNTAX_STYLE_LUA // HRM more closely matches lua than assembly
+			tabSize = 4
+		}
+		
+		frame.run {
+			defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+			add(RTextScrollPane(textArea).apply {
+				lineNumbersEnabled = true
+			})
+			pack()
+			setSize(400, 600)
+			isVisible = true
+			setLocationRelativeTo(null)
+		}
+		
+	}
+	
+}
